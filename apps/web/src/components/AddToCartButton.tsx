2cb7a4c067c51d9a51550c301782df7ec281dedb
dmobley0608 <dmobley0608@gmail.com>
2026-04-19 09:39:00 -0400

    feat(shop): add product details page, AddToCartButton component, and not-found page. update product list page  and product card

---
+++ apps/web/src/components/AddToCartButton.tsx
@@ -0,0 +1,53 @@
+'use client';
+import React from 'react';
+import { Button } from '@/components/ui';
+import { useCart } from '@/store/cart';
+
+
+type AddToCartButtonProps = {
+  productId: string;
+  name: string,
+  price: number
+};
+
+export const AddToCartButton = ({ productId, name, price }: AddToCartButtonProps) => {
+
+  const items = useCart((state) => state.items);
+
+  const addItem = useCart((state) => state.addItem);
+  const updateQuantity = useCart((state) => state.updateQuantity);
+
+  const inCart = items.filter((i) => i.productId === productId)[0];
+  return (
+    <div className="flex flex-col">
+      {inCart && (
+        <div className="mb-3">
+          <span>You have</span>
+          <Button
+            onClick={() => updateQuantity(productId, inCart.quantity - 1)}
+            size="sm"
+            variant="accent"
+            className="mx-3"
+          >
+           -
+          </Button>
+          <span>{inCart.quantity}</span>
+          <Button
+            onClick={() => updateQuantity(productId, inCart.quantity + 1)}
+            size="sm"
+            variant="accent"
+            className="mx-3"
+          >
+            +
+          </Button>
+          <span>in your cart</span>
+        </div>
+      )}
+      {!inCart && (
+        <Button onClick={() => addItem({ productId, name, price })}>
+          Add To Cart
+        </Button>
+      )}
+    </div>
+  );
+};
